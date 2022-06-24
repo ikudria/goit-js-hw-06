b57ca2d83ecd2162cf160f678e5ae0b947503dfd
ikudria <irakudrya@gmail.com>
2022-06-24 04:52:30 -0500

    Task-02_done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add(`item`);
+  listItem.textContent = ingredient;
+  return listItem;
+ }
+)
+
+// console.log(elements);
+
+listEl.append(...elements);
+console.log(listEl);
+
