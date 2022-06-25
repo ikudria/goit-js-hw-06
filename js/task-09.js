0a685ead77045ab6c096c1f7ecfa154bb6be1632
ikudria <irakudrya@gmail.com>
2022-06-25 14:06:56 -0500

    Task-09_done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body');
+const colorBtn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+
+
+colorBtn.addEventListener('click', onColorBtnClick); 
+
+function onColorBtnClick() {
+  
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = body.style.backgroundColor;  
+  
+}
